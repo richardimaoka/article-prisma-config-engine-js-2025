566f624f7813ad9f14ba08b86ac0dce31c29f3cd
richardimaoka <richard.s.imaoka@gmail.com>
2025-11-16 21:10:10 +0900

    add adapter

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,5 +1,8 @@
 import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
+import { PrismaBetterSQLite3 } from "@prisma/adapter-better-sqlite3";
+
+const adapter = new PrismaBetterSQLite3({ url: process.env.DATABASE_URL });
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
@@ -7,4 +10,5 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   engine: "js",
+  adapter: () => Promise.resolve(adapter),
 });
